66147d9ec5802b82c6f6b58f344f38f5511e6916
Dilshan <dilshanwijerathna426@gmail.com>
2025-10-29 22:44:10 +0530

    myLast Commit

---
+++ AuctionApi/AuctionApi/AuctionApi/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+namespace AuctionApi.Models
+{
+    
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "User";
+        public List<AuctionItem> AuctionItems { get; set; } = new();
+        public List<Bid> Bids { get; set; } = new();
+    }
+}
